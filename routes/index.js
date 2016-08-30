4d626b0f1f838e82cf47e5b8b975cbdf271d3621
benjamin-white <benjaminwhite@live.co.uk>
2016-08-30 18:22:32 +0100

    initial Mocha tests

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,23 @@ router.get('/', function (req, res) {
 });
 
 router.get('/register', function(req, res) {
+    if (req.user) res.redirect('/');
     res.render('register', { });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', function(req, res, next) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+          return res.render('register', { error : err.message });
         }
 
         passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
         });
     });
 });
